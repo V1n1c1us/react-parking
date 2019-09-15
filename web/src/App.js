e41be13bea24c73af4f1b2c03de042752e8414fb
V1n1c1us <v.f.diehl@gmail.com>
2019-09-15 13:33:11 -0300

    Web

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header/index'
+function App() {
+  return (
+    <div className="main-container">
+      <Header title="Estacionamento" />
+    </div>
+  );
+}
+
+export default App;
